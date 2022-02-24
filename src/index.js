e84f479b8b7fe9a62f2750a2c8300cb1f0afe653
grshkvch <volody_200115@mail.ru>
2022-02-24 19:33:55 +0300

    feat: solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+  let result = '';
+  let nString = n.toString()
+    if (nString.startsWith('-')) {
+        nString = nString.slice(1)
+    }
+    for (let i = 0; i < nString.length; i++) {
+        result = nString[i] + result
+    }
+return Number(result);
 }
